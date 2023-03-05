2fa7e7d235281da75ef93d35ab034c640b26bcc0
Lincoln Lee <Lsha0730@gmail.com>
2023-03-05 04:09:40 -0800

    feat: complete write operation

---
+++ util/jsonUtils.go
@@ -0,0 +1,83 @@
+package util
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
+// Takes a struct and injects the given data at the specified location.
+func InjectData(target map[string]interface{}, path string, value interface{}) map[string]interface{} {
+	pathArr := strings.Split(path, "/")
+	if pathArr[0] == "" {
+		pathArr = pathArr[1:]
+	}
+	if pathArr[len(pathArr)-1] == "" {
+		pathArr = pathArr[:len(pathArr)-1]
+	}
+
+	return traverse(target, pathArr, value).(map[string]interface{})
+}
+
+// Takes a node and overwrites value at specified path with given value
+func traverse(node interface{}, pathArr []string, value interface{}) interface{} {
+	if len(pathArr) == 0 {
+		return value
+	}
+	target := pathArr[0]
+
+	if _, ok := node.(map[string]interface{}); !ok {
+		// Primitive
+		if len(pathArr) != 0 {
+			return makeObj(pathArr, value)
+		} else {
+			return value
+		}
+	}
+
+	// Object
+	result := map[string]interface{}{}
+	castedNode := node.(map[string]interface{})
+	targetFound := false
+	for key := range castedNode {
+		if key == target {
+			result[key] = traverse(castedNode[key], pathArr[1:], value)
+			targetFound = true
+		} else {
+			result[key] = castedNode[key]
+		}
+	}
+
+	if !targetFound {
+		result[target] = traverse(castedNode[target], pathArr[1:], value)
+	}
+
+	return result
+}
+
+// Returns a nested object structure according to the path array, with the given value attached at the end
+func makeObj(pathArr []string, value interface{}) interface{} {
+	if len(pathArr) == 0 {
+		return value
+	}
+
+	result := map[string]interface{}{}
+	if len(pathArr) == 1 {
+		result[pathArr[0]] = value
+	} else {
+		result[pathArr[0]] = makeObj(pathArr[1:], value)
+	}
+
+	return result
+}
+
+func printJSON(input map[string]interface{}) {
+	jsonString, err := json.Marshal(input)
+	if err != nil {
+		fmt.Println("Error marshaling JSON:", err)
+		return
+	}
+
+	// Print the JSON string to the console
+	fmt.Println(string(jsonString))
+}
--- util/queryHandlers.go
+++ util/queryHandlers.go
@@ -2,8 +2,10 @@ package util
 
 import (
 	"encoding/json"
+	"errors"
 	"io/ioutil"
 	"os"
+	"strings"
 )
 
 var DBROOT string = "./data"
@@ -28,18 +30,39 @@ func (d *Driver) HandleRead(path string) error {
 }
 
 func (d *Driver) HandleWrite(path string, value interface{}) error {
-	os.Mkdir(DBROOT, 0644)
+	// Check not writing to root
+	pathArr := strings.Split(path, "/")
+	if pathArr[0] == "" {
+		pathArr = pathArr[1:]
+	}
+	if pathArr[len(pathArr)-1] == "" {
+		pathArr = pathArr[:len(pathArr)-1]
+	}
+	if len(pathArr) == 0 {
+		return errors.New("ERROR: Cannot write to root")
+	}
 
-	finalPath := d.root
+	// Ensure data directory exists
+	os.Mkdir(DBROOT, 0644)
+	finalPath := d.root + ".json"
 	tempPath := finalPath + ".tmp"
 
-	b, err := json.MarshalIndent(value, "", "\t")
-	if err != nil {
-		return err
+	// Get the contents
+	original := map[string]interface{}{}
+	b1, err1 := ioutil.ReadFile(finalPath)
+	if err1 == nil {
+		json.Unmarshal(b1, &original)
 	}
-	b = append(b, byte('\n'))
+	toWrite := InjectData(original, path, value)
 
-	if err := ioutil.WriteFile(tempPath, b, 0644); err != nil {
+	// Write the contents
+	b2, err2 := json.MarshalIndent(toWrite, "", "\t")
+	if err2 != nil {
+		return err2
+	}
+	b2 = append(b2, byte('\n'))
+
+	if err := ioutil.WriteFile(tempPath, b2, 0644); err != nil {
 		return err
 	}
 
